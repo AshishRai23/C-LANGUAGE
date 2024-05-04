aed7999ff3cd958ec877f978a782ba8fc61008a2
Ashish Rai <ar5605106@gmail.com>
2024-05-04 12:52:50 +0530

    Create for_loop.c

---
+++ for_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//for loop note 
+/*the syntax of for loop like this for (initilage ; test; increamentor decreament)*/
+
+
+int main(){
+    for(int a = 0; a<10; a++){
+        printf("the value of a is %d \n", a);
+    }
+    return 0;
+}
